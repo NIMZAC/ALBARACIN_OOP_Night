a0a2b604acb22d929f70970827a9eeb9deef7a97
NIMZAC <znimac783@gmail.com>
2024-03-07 00:06:42 +0800

    ALBARACIN_NIMROLD

---
+++ AlbaracinNimrold/src/main/Student.java
@@ -0,0 +1,34 @@
+
+package main;
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean isMinor;
+
+    
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.isMinor = age < 18;
+    }
+
+  
+    public String getName() {
+        return lastName + ", " + firstName;
+    }
+
+   
+    public void increaseAge() {
+        age++;
+        isMinor = age < 18;
+    }
+
+  
+    public String toString() {
+        String status = isMinor ? "minor" : "adult";
+        return getName() + " - " + age + " - " + status;
+    }
+}
+    
